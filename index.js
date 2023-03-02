c7b4d5e13a9506a78f3b55fd56102bf7b756a361
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-02 23:56:16 +0000

    Rearrange questions

--- index.js
+++ index.js
@@ -9,48 +9,59 @@ const questions = [
     {
         type: "input",
         name: "github",
-        message: "What is your GitHub username?"
+        message: 'What is your GitHub username?',
     },
     {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
     },
     {
-        type: "input",
-        name: "title",
-        message: "Tell us your project name"
+      type: 'list',
+      name: 'license',
+      message: 'Tell us what license your project has:',
+      choices: [
+        'Boost1.0',
+        'MIT',
+        'APACHE2.0',
+        'CDDL1.0',
+        'BSD2',
+        'BSD3',
+        'GPL3.0',
+        'None',
+      ],
     },
     {
-        type: "input",
-        name: "title",
-        message: "Describe what your project is about:"        
+      type: 'input',
+      name: 'title',
+      message: 'Tell us your project name',
     },
     {
-        type: "list",
-        name: "license",
-        message: "Tell us what license your project has:",
-        choices: ["Boost1.0", "MIT", "APACHE2.0", "CDDL1.0", "BSD2", "BSD3", "GPL3.0", "None"]
+      type: 'input',
+      name: 'description',
+      message: 'Describe what your project is about:',
     },
     {
-        type: "input",
-        name: "Contributors",
-        message: "Who else contributed to this project?"
-    
+      type: 'input',
+      name: 'installation',
+      message: 'How to install your application?',
     },
     {
-        type: "input",
-        name: "reponame",
-        message: "What is your repository name for the ReadME?"
+      type: 'input',
+      name: 'usage',
+      message: 'How to use your application?',
     },
     {
-        type: "input",
-        name: "usage",
-        message: "How is application being used?"
+      type: 'input',
+      name: 'contribution',
+      message: 'How can others contribute to your project?',
     },
-
-
-];
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Describe tests for your application',
+    },
+  ];
 
 // function to write README file
 function writeToFile(fileName, data) {
